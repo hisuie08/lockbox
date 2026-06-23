f45978ab6a417b2c546ee1e418b684d8f2a2dfe9
hisuie08 <keisyu0825@gmail.com>
2026-06-23 02:43:11 +0000

    fix vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:"/lockbox/",
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
